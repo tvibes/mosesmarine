37971f02ac774e1758b1a2064df3f7618886ee67
tvibes <tvibesexpress@gmail.com>
2019-12-17 20:38:57 +0100

    first push ready

--- mosesmarine/settings/local.py
+++ mosesmarine/settings/local.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     
+    # Whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mosesmarine.urls'
